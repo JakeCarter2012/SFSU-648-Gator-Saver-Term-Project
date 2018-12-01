35100d30268cdca28b95214e7f7d54975d1ca328
Jake Carter <jakecarter2012@me.com>
2018-11-30 16:59:50 -0800

    Added admin and messages tables to db

--- sfsuListings/__init__.py
+++ sfsuListings/__init__.py
@@ -2,7 +2,9 @@ import logging
 import base64
 
 import sqlite3
+import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
 from flask import Flask, flash, redirect, render_template, request, session, abort, g, Blueprint
 
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -40,6 +42,7 @@ class Posts(db.Model):
     price = db.Column(db.INTEGER, unique=False, nullable=False, primary_key=True)
     image = db.Column(db.BLOB, unique=False, nullable=True, primary_key=False)
     category = db.Column(db.String(80), unique=False, nullable=False, primary_key=False)
+    date = db.Column(DateTime, default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return "<Name: {}>".format(self.name)
@@ -48,7 +51,6 @@ class Posts(db.Model):
         return "<price: {}".format(self.price)
         return "<image: {}".format(self.image)
         return "<category: {}".format(self.category)
-
 
 '''    
 Class for registered user; note that passord is hashed
@@ -67,6 +69,30 @@ class RegisteredUser(db.Model):
         return "<Username: {}>".format(self.UserName)
         return "<Hashed Password: {}".format(self.password_hash)
 
+class Admin(db.Model):
+    AdminName = db.Column(db.String(30), unique=True, nullable=False, primary_key=True)
+    password_hash = db.Column(db.String(96), unique=True, nullable=False, primary_key=False)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<Username: {}>".format(self.UserName)
+        return "<Hashed Password: {}".format(self.password_hash)
+
+class Messages(db.Model):
+    id = db.Column(db.INTEGER, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    sentFrom = db.Column(db.String(30), unique=False, nullable=False, primary_key=False)
+    sentTo = db.Column(db.String(30), unique=False, nullable=False, primary_key=False)
+    postId = db.Column(db.INTEGER, unique=True, nullable=False, primary_key=False)
+    message = db.Column(db.String(300), unique=False, nullable=False, primary_key=False)
+    date = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return "sentFrom: {}".format(self.sentFrom)
+        return "sentTo: {}".format(self.sentTo)
+        return "postId: {}".format(self.postId)
+        return "message: {}".format(self.message)
 
 # index page
 @app.route('/', methods=["GET", "POST"])
