7acd1c64a59a930882b0487453c3fff1c734ee31
Alvin <alvinrobertlee@gmail.com>
2018-12-16 15:41:59 -0800

    Moved all about pages into an about folder, edited the routes to account for this change. Changed text of the about page

--- sfsuListings/aboutPage.py
+++ sfsuListings/aboutPage.py
@@ -1,42 +1,42 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, g, Blueprint
+from flask import Flask, url_for, flash, redirect, render_template, request, session, abort, g, Blueprint
 aboutPage = Blueprint('about_page', __name__, template_folder='templates')
 
 # about page
 @aboutPage.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about/about.html')
 
 
-@aboutPage.route('/aboutPage.jake')
+@aboutPage.route('/about/jake')
 def jake():
-    return render_template('aboutJake.html')
+    return render_template('about/jake.html')
 
 
 @aboutPage.route('/aboutPage.gary')
 def gary():
-    return render_template('garyd.html')
+    return render_template('about/gary.html')
 
 
 @aboutPage.route('/aboutPage.martin')
 def martin():
-    return render_template('MartinLeeAboutPage.html')
+    return render_template('about/martin.html')
 
 
 @aboutPage.route('/aboutPage.wagner')
 def wagner():
-    return render_template('WagnerAbout.html')
+    return render_template('about/wagner.html')
 
 
 @aboutPage.route('/aboutPage.gordon')
 def gordon():
-    return render_template('Gordon about page.html')
+    return render_template('about/gordon.html')
 
 
 @aboutPage.route('/aboutPage.tina')
 def tina():
-    return render_template('About Tina.html')
+    return render_template('about/tina.html')
 
 
 @aboutPage.route('/aboutPage.alvin')
 def alvin():
-    return render_template('aboutAlvin.html')
+    return render_template('about/alvin.html')
