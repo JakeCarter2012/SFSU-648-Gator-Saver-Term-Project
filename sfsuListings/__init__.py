484fb650f3c8cce36c1fb82ea9c7e4345b165b9c
Alvin <alvinrobertlee@gmail.com>
2018-12-16 17:08:16 -0800

    removed main from init

--- sfsuListings/__init__.py
+++ sfsuListings/__init__.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
 from flask import Flask, flash, redirect, render_template, request, session, abort, g, Blueprint, url_for, send_file
 
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
 from flask_migrate import Migrate
 import os
 
@@ -30,18 +28,10 @@ app.register_blueprint(dashboard)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 # NOTE: Secret key resets to new key each time server is restarted;
 # this will invalidate any old session the user has, and requires log out
 # can hardcode the secret key to 'solve' this, but is considered unsafe practice
 app.config['SECRET_KEY'] = os.urandom(24)
-
-
-if __name__ == '__main__':
-    app.debug = True  # Turn on auto reloader and debugger
-    app.config['SQLALCHEMY_ECHO'] = True  # Show SQL commands created
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.run()
 
 from sfsuListings import models, routes
